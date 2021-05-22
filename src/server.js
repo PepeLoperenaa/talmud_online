e9215ab0b46349c88847fbb3009419fab015a3bc
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-22 17:44:54 +0100

    make call into index.html

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000')
+})
+
+const alert = "Welcome to the game";
+function fadeIn(){
+    $('OnlinePlayer').on({
+        'click': function (){
+            $('player1Cards').fadeIn("slow");
+        }
+    })
+}
+function playerOn() {
+    window.alert(alert);
+    const c = document.getElementById('player1Cards');
+    c.setAttribute("style", "visibility:visible"); //dont know if jquery code goes here for the fade in.
+}
