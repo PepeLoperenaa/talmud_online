3dce271ebe00d85259b4060fd1f0331487d5ad2c
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-22 18:17:24 +0100

    fix html page and try to make css work

--- src/server.js
+++ src/server.js
@@ -3,15 +3,19 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
+app.use(express.static(__dirname + '/static'))
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+
 
 server.listen(3000, () => {
     console.log('listening on *:3000')
 })
 
-const alert = "Welcome to the game";
+/*const alert = "Welcome to the game";
 function fadeIn(){
     $('OnlinePlayer').on({
         'click': function (){
@@ -23,4 +27,4 @@ function playerOn() {
     window.alert(alert);
     const c = document.getElementById('player1Cards');
     c.setAttribute("style", "visibility:visible"); //dont know if jquery code goes here for the fade in.
-}
+}*/
