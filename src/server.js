e9ce3247b0a8c85174136f8bcd3ce4e0b299d01c
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-05-24 09:57:13 +0100

    css is connected, now images

--- src/server.js
+++ src/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const tools = require('src/talmud.js');
 
 app.use(express.static(__dirname + '/static'))
+
+app.use('/images', express.static(path.join(__dirname, 'naipes')));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
-
-
 
 server.listen(3000, () => {
     console.log('listening on *:3000')
--- src/talmud.js
+++ src/talmud.js
@@ -1,15 +1,1 @@
-const alert = "Welcome to the game";
-
-
-function fadeIn(){
-    $('OnlinePlayer').on({
-        'click': function (){
-            $('player1Cards').fadeIn("slow");
-        }
-    })
-}
-function playerOn() {
-    window.alert(alert);
-    const c = document.getElementById('player1Cards');
-    c.setAttribute("style", "visibility:visible"); //dont know if jquery code goes here for the fade in.
-}
+const hello = "helpo"
