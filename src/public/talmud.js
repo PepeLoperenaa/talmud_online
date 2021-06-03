ec5ae8f9836a63ec0b3089c8ff00236df5b43ecd
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-06-03 18:31:04 +0100

    implemented changing cards correctly

--- src/public/talmud.js
+++ src/public/talmud.js
@@ -1,45 +1,22 @@
 const alert = "Welcome to the game";
-
+//client page. each variable is different for every player
+var last_action = "";
+var socket;
 var game_status = {};
 var me = null;
 var open_chairs = 1;
 
-var socket = io.connect('http://localhost:3000', {
-    'forceNew': true
-});
+//steps 1: funcion en HTMl
+// 2: hacer funcion en cliente
+// 3: socket.emit al servidor
+//4: socket.on en el servidor
+// 5:socket.on en el cliente
 
-socket.on('game_status', function (data) {
-    game_status = data;
-    enable_buttons(data.turn === me.name);
-});
-
-socket.on('error', function (data) {
-    window.alert("Full room!");
-});
-
-socket.on('new_status', function (data) {
-    me = data.player_info;
-    renderStatus(data);
-});
-
-socket.on('get_card_response', function (data) {
-    me = data;
-    console.log("get_card_response: " + data);
-    updateStatus(data);
-});
-
-socket.on('player_accepted', function (data) {
-    renderNewPlayer();
-});
-
-function renderNewPlayer() {
+function renderNewPlayer() { // When new players come in, then their cards are rendered into the game.
     open_chairs++;
     console.log("opened chair number " + open_chairs);
     var div = document.getElementById("player" + open_chairs + "Cards");
-    div.style.visibility = 'visible'; //if a new player comes into the game, then show new cards on the main page.
-    /*
-    Need to add the ID of the player so that when a player comes into the game, the other cards are already added if needed.
-     */
+    div.style.visibility = 'visible';
 }
 
 function renderStatus(data) {
@@ -57,8 +34,8 @@ function renderStatus(data) {
     }
     document.getElementById('OnlinePlayer').style.visibility = "hidden";
 
-    // open as many chais as number of players in
-    for (var i = 0; i < data.num_players - 1; i++) {
+    // open as many chairs as number of players in
+    for (var j = 0; j < data.num_players - 1; j++) {
         renderNewPlayer();
     }
 }
@@ -70,26 +47,33 @@ function updateStatus(data) {
         var div = document.getElementById("card" + i);
         if (data.cards[i - 1].status === "visible") {
             var path = "/" + data.cards[i - 1].value;
-            div.innerHTML = "<img src='" + path + "' alt=\"card\">";
+            div.innerHTML = "<img src='" + path + "' alt=\"card\">"; //2 cartas con valor
         } else {
-            div.innerHTML = "<img src='naipes/reves.png' alt=\"card\">"
+            div.innerHTML = "<img src='naipes/reves.png' alt=\"card\">" //2 cartas que no se saben el valor
         }
     }
 
 }
 
 function playerOn() {
+    prepareGame();
+
     const c = document.getElementById('player1Cards');
-    c.setAttribute("style", "visibility:visible"); //dont know if jquery code goes here for the fade in.
+    c.setAttribute("style", "visibility:visible");
     socket.emit('new_player', 'access');
     return false;
 }
 
-// pedir carta al servidor, indicando quien soy
-function getCard() {
+function showDeck() {
+    // request view deck card
+    socket.emit('request_deck_card', ""); //esto es lo que se envia al servidor
+}
 
+function getCard() { //when we get the card, show what the value of the card is before changing it.
+    //var new_card = switchCard
     var index = window.prompt("Choose one of your cards (1-" + me.cards.length + ")", "-1");
-    if (index === "-1") {
+    if(index===null || index==="-1") {
+        document.getElementById("available_card").src = "naipes/reves.png";
         return;
     }
 
@@ -100,12 +84,88 @@ function getCard() {
         index_change: index
     }
 
+    // hide again deck
+    document.getElementById("available_card").src = "naipes/reves.png";
+
     socket.emit('get_card', change);
 
-    document.getElementById("pushed_card").src = me.cards[index].value;
-    document.getElementById("oldDeck").style.visibility = "visible";
 }
+
+function moveCardToPushed() {
+    socket.emit('move_card_to_pushed', "");
+    //socket.emit = hacer llamada al servidor
+}
+
 
 function enable_buttons(active) {
     // enable/disable buttons
+}
+
+function prepareGame() {
+    //socket.on = respuesta del servidor para hacer una accion / visualizacion etc.
+    socket = io.connect('http://localhost:3000', {
+        'forceNew': true
+    });
+
+    socket.on('game_status', function (data) {
+        game_status = data;
+        enable_buttons(data.turn === me.name);
+    });
+
+    socket.on('error', function (data) {
+        window.alert("Full room!");
+    });
+
+    socket.on('new_status', function (data) {
+        me = data.player_info;
+        renderStatus(data);
+    });
+
+    socket.on('get_card_response', function (data) {
+        me = data;
+        console.log("get_card_response: " + data);
+        updateStatus(data);
+    });
+
+    socket.on('new_pushed_card', function (data) {
+        console.log('new_pushed card ' + data);
+        document.getElementById("oldDeck").style.visibility = "visible";
+        document.getElementById("pushed_card").src = data;
+    });
+
+    socket.on('player_accepted', function (data) {
+        renderNewPlayer();
+    });
+
+    socket.on('response_deck_card', function (data) {
+        console.log("card on deck: "+ data);
+        document.getElementById("available_card").src = data;
+        document.getElementById("message").innerHTML = "Change card?";
+        last_action = "showDeck";
+        enable_yes_no_buttons(true);
+    });
+}
+
+function enable_yes_no_buttons(show) {
+    document.getElementById("yes").style.visibility = show ? "visible" : "hidden";
+    document.getElementById("no").style.visibility = show ? "visible" : "hidden";
+}
+
+function yes() {
+    enable_yes_no_buttons(false);
+    document.getElementById("message").innerHTML = ""
+    if(last_action==="showDeck") {
+        getCard();
+    }
+    last_action = "";
+}
+
+function no() {
+    enable_yes_no_buttons(false);
+    document.getElementById("message").innerHTML = ""
+    if(last_action==="showDeck") {
+        moveCardToPushed();
+        document.getElementById("available_card").src = "naipes/reves.png";
+    }
+    last_action = "";
 }
--- src/server/server.js
+++ src/server/server.js
@@ -18,9 +18,12 @@ var new_cards = ["naipes/1Oro.png", "naipes/2Oro.png", "naipes/3Oro.png", "naipe
     "naipes/1Baston.png", "naipes/2Baston.png", "naipes/3Baston.png", "naipes/4Baston.png", "naipes/5Baston.png", "naipes/6Baston.png", "naipes/7Baston.png", "naipes/8Baston.png", "naipes/9Baston.png", "naipes/10Baston.png", "naipes/11Baston.png", "naipes/12Baston.png",]; //cards to use
 var push_cards = []; // cards which are send to players
 
+// shuffle cards
+shuffle(new_cards);
 //make the cards objects to get the value of the different cards easier.
 
 app.use(express.static('public'));
+
 
 io.on('connection', function (socket) {
     console.log('A user has connected to the server with the ID: ' + socket.id);
@@ -35,14 +38,14 @@ io.on('connection', function (socket) {
         // randomize cards
         var c = [];
         for (var i = 0; i < 4; i++) {
-            let pos = Math.floor(Math.random() * (new_cards.length));
+            let pos = 0;
             var card = {
                 value: new_cards[pos],
                 status: (i < 2) ? "hidden" : "visible"
             };
             c.push(card);
             // remove from deck
-            new_cards.slice(pos, 1);
+            new_cards.splice(pos, 1);
             //console.log("Remaining cards: "+new_cards.length);
         }
 
@@ -61,9 +64,9 @@ io.on('connection', function (socket) {
         }
 
         // send him info and cards
-        socket.emit('new_status', info);
+        socket.emit('new_status', info); //especificamente el que se ha comunicado con el servidor
         // send rest player accepted
-        socket.broadcast.emit('player_accepted', "");
+        socket.broadcast.emit('player_accepted', ""); //broadcast = todos los clientes menos el que se ha comunicado
 
         if (players.length === 4) {
             game_status.status = "started";
@@ -76,6 +79,26 @@ io.on('connection', function (socket) {
         switchCard(data.player.name, data.index_change);
         // send him info and cards
         socket.emit('get_card_response', players[getPlayerIndex(data.player.name)]);
+
+        // send all players new pushed card
+        io.emit('new_pushed_card', push_cards[push_cards.length-1]); //enviar informacion a todos (incluido el que manda)
+        //socket.emit('new_pushed_card', push_cards[push_cards.length-1]);
+        //socket.broadcast.emit('new_pushed_card', push_cards[push_cards.length-1]);
+    });
+
+    socket.on('request_deck_card', function (data) {
+        socket.emit('response_deck_card', new_cards[0]);
+        //socket.on hace la la tarea que sea con la informacion pasada
+        //y responde lo que tenga que saber al cliente para poder actualizar lo que ve el cliente.
+    });
+
+    socket.on('move_card_to_pushed', function (data) {
+        var card = new_cards[0];
+        new_cards.splice(0, 1);
+        push_cards.push(card);
+        console.log("length new_cards : " + new_cards.length);
+        console.log("new_cards top : " +new_cards[0]);
+        io.emit('new_pushed_card', push_cards[push_cards.length-1]);
     });
 });
 
@@ -95,9 +118,9 @@ function getPlayerIndex(name) {
 function switchCard(name, index_change) {
     var index = getPlayerIndex(name);
     var player_card = players[getPlayerIndex(name)].cards[index_change];
-    let pos = Math.floor(Math.random() * (new_cards.length));
-    var random_card = new_cards[pos];
-    new_cards.slice(pos, 1);
+    let pos = 0;
+    var random_card = new_cards[0];
+    new_cards.splice(pos, 1);
     push_cards.push(player_card.value);
     var card = {
         value: random_card,
@@ -106,3 +129,21 @@ function switchCard(name, index_change) {
     players[getPlayerIndex(name)].cards[index_change] = card;
 }
 
+// shuffle array of cards
+function shuffle(array) {
+    var currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
